dc956552a5e3b1382305fa0342124917c0797bae
Thoma411 <nadtm_thomax11@163.com>
2026-08-13 00:53:21 +0800

    chore: 更新正式版本号

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: Thoma4
  * @Date: 2026-06-24 00:17:53
- * @LastEditTime: 2026-08-11 23:26:48
+ * @LastEditTime: 2026-08-13 00:51:43
  * @Description: 设置页
  */
 
@@ -45,7 +45,7 @@ class SettingsPageState extends State<SettingsPage> {
   bool _autoSyncEnabled = false; // 静默同步
   String _appPath = "";
 
-  static const String currentVersion = "v1.1.1";
+  static const String currentVersion = "v1.1.2";
 
   @override
   void initState() {
